ea7bec5110fbb76413cf0b9efdbef513b33a855e
JakeMustard <jacobhuskinsoncode@gmail.com>
2023-08-08 11:26:32 +0800

    Adding the PDF

--- src/week5homework/App.java
+++ src/week5homework/App.java
@@ -5,11 +5,11 @@ public class App { // Defines a class named App.
     public static void main(String[] args) { // The main method, the entry point of the application.
         Logger asteriskLogger = new AsteriskLogger(); // Creates an instance of the AsteriskLogger class and assigns it to the asteriskLogger variable.
         asteriskLogger.log("Hello"); // Calls the log method of the asteriskLogger instance with the message "Hello".
-        asteriskLogger.error("Something went wrong"); // Calls the error method of the asteriskLogger instance with the error message "Something went wrong".
+        asteriskLogger.error("Hello"); // Calls the error method of the asteriskLogger instance with the error message "Something went wrong".
 
         Logger spacedLogger = new SpacedLogger(); // Creates an instance of the SpacedLogger class and assigns it to the spacedLogger variable.
         spacedLogger.log("Hello"); // Calls the log method of the spacedLogger instance with the message "Hello".
-        spacedLogger.error("Something went wrong"); // Calls the error method of the spacedLogger instance with the error message "Something went wrong".
+        spacedLogger.error("Hello"); // Calls the error method of the spacedLogger instance with the error message "Something went wrong".
     }
 }
 
